bcdea323cc1902ea8dc424fd749fc8476ecde7b3
thichcodedao2006 <truongpd.a2.2124@gmail.com>
2026-09-15 14:13:03 +0700

    feat: Game polish

--- scenes/main.gd
+++ scenes/main.gd
@@ -12,6 +12,8 @@ func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
+	$Music.stop()
+	$DeathSound.play()
 
 func new_game():
 	score = 0
@@ -19,6 +21,7 @@ func new_game():
 	$StartTimer.start()
 	$HUD.update_score(score)
 	$HUD.show_message("Get Ready")
+	$Music.play()
 	get_tree().call_group("mobs", "queue_free")
 
 
